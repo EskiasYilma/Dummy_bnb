72b7c9caa4f98e1a425577aa86a31f2c43251928
Mohammed_Android <mohammedthe11th@gmail.com>
2023-03-07 07:54:52 +0300

    BaseModel unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -45,7 +45,7 @@ class TestBase_Init(unittest.TestCase):
         self.assertNotEqual(updated_at, model.updated_at)
 
     def test_to_dict(self):
-        """Unittest for testing the test_to_dict returns a dictionary and all\
+        """Unittest for testing the to_dict method returns a dictionary and all\
                 requiered elements are presented"""
 
         model = BaseModel()
@@ -55,3 +55,15 @@ class TestBase_Init(unittest.TestCase):
         self.assertIn('id', dict_model)
         self.assertIn('created_at', dict_model)
         self.assertIn('updated_at', dict_model)
+
+    def test_kwargs(self):
+        """Unittest for testing proper handling of kwargs"""
+        kwargs = {"id": "12345", "created_at": "2023-01-15T01:30:40.000000",
+                  "updated_at": "2023-01-15T01:30:45.000000", "name": "BaseModel", "value": 1234}
+        model = BaseModel(**kwargs)
+        self.assertEqual(model.id, kwargs["id"])
+        self.assertEqual(model.created_at, datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertEqual(model.updated_at, datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertEqual(model.__dict__["name"], kwargs["name"])
+        self.assertEqual(model.__dict__["value"], kwargs["value"])
+
