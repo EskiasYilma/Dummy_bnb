e6cf498eca98fa0e41377f5fceb667c8b094cdc7
gHost <eskiasyt@gmail.com>
2023-03-08 12:13:18 +0300

    task 7 init

--- console.py
+++ console.py
@@ -1,24 +1,78 @@
 #!/usr/bin/python3
 """
 Console Module Docstring
+
+Defines the entry point of the command interpreter
 """
 
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
 	"""
-	TODO
+	HBNBCommand Class Docstring
+
+	Defines the entry point of the command interpreter
 	"""
-	def do_quit(self, inp):
+	__all_models = {"BaseModel": BaseModel}
+
+	prompt = "(hbnb) "
+	def do_quit(self, arg):
 		"""Quit command to exit the program\n"""
 		return True
 
-	def do_EOF(self, inp):
+	def do_EOF(self, arg):
 		"""Quit command to exit the program\n"""
 		return True
 
 	def emptyline(self):
-		pass		
+		pass
+
+	def do_create(self, arg):
+		"""Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id.\nUsage:\n\tcreate <model_name>
+		"""
+		# self.__all_models = {"BaseModel": BaseModel}
+
+		if arg:
+			if str(arg) in self.__all_models.keys():
+				model = BaseModel()
+				model.save()
+				print(model.id)
+			else:
+				print("** class doesn't exist **")
+		else:
+			print("** class name missing **")
+
+	def do_show(self, arg):
+		"""
+		test
+		"""
+		objects = {}
+		if arg:
+			if len(str(arg).split(" ")) == 2:
+				cls, id = str(arg).split(" ")
+				# id = str(arg).split(" ")[1]
+				if cls not in self.__all_models.keys():
+					print("** class doesn't exist **")
+				if id not in [y.id for x, y in storage.all().items()]:
+					# print(self.__all_models[cls].__objects)
+					for i,j in storage.all().items():
+						print(i, j)
+
+
+			if len(str(arg).split(" ")) == 1:
+				cls = str(arg).split(" ")[0]
+				# id = str(arg).split(" ")[1]
+				if cls not in self.__all_models.keys():
+					print("** class doesn't exist **")
+				else:
+					print("** instance id missing **")
+
+		else:
+			print("** class name missing **")
+
 
 
 
