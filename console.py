d43fd46be1713df0e9ad81bb1068148716deb3bb
gHost <eskiasyt@gmail.com>
2023-03-08 11:02:48 +0300

    Task 6

--- console.py
+++ console.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
 """
-Module Docstring
+Console Module Docstring
 """
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+	"""
+	TODO
+	"""
+	def do_quit(self, inp):
+		"""Quit command to exit the program\n"""
+		return True
+
+	def do_EOF(self, inp):
+		"""Quit command to exit the program\n"""
+		return True
+
+	def emptyline(self):
+		pass		
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
